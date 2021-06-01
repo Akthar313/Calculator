2d7307b4c2f74d8545fb05ecbeaa3eef938be533
h4ck€r313 <48244891+Akthar303@users.noreply.github.com>
2021-06-01 19:55:41 +0530

    Updated
    Added condition when entering the operator

--- calculator.py
+++ calculator.py
@@ -1,49 +1,44 @@
-""" Simple calculator program """
+#simple calculator
 
-"""Start of functions of Operators"""
+def add(num1,num2):
+    return num1+num2
+    
+def sub(num1,num2):
+    return num1-num2
+    
+def mul(num1,num2):
+    return num1*num2
+    
+def div(num1,num2):
+    return num1/num2
+    
+def mod(num1,num2):
+    return num1%num2
+    
+print("Select the operator\n"
+        "1> Add\n"
+        "2> Sub\n"
+        "3> Mul\n"
+        "4> div\n"
+        "5> mod\n")
+        
+select = int(input("Select 1,2,3,4,5:\n"))
 
-
-def add(num1, num2):
-    return num1 + num2
-
-
-def sub(num1, num2):
-    return num1 - num2
-
-
-def mul(num1, num2):
-    return num1 * num2
-
-
-def div(num1, num2):
-    return num1 / num2
-
-
-"""End of functions of Operators"""
-
-
-print("Select the operation- \n"
-      "1. Addition \n"
-      "2. Subtraction \n"
-      "3. Multiplication \n"
-      "4. Division \n")
-
-select = int(input("Select the operation from  1, 2, 3, 4 :"))
-
-number1 = float(input("Enter the first number :"))
-number2 = float(input("Enter the second number :"))
-
-if select == 1:
-    print(number1, "+", number2, "=", add(number1, number2))
-
-elif select == 2:
-    print(number1, "+", number2, "=", sub(number1, number2))
-
-elif select == 3:
-    print(number1, "+", number2, "=", mul(number1, number2))
-
-elif select == 4:
-    print(number1, "+", number2, "=", div(number1, number2))
-
+if(select == 1 or select == 2 or select == 3 or select == 4 or select == 5):
+    x = float(input("Enter first number\n"))
+    y = float(input("Enter second number\n"))
 else:
     print("Invalid Operator")
+    
+
+if(select == 1):
+    print(add(x,y))
+elif(select == 2):
+    print(sub(x,y))
+elif(select == 3):
+    print(mul(x,y))
+elif(select == 4):
+    print(div(x,y))
+elif(select == 5):
+    print(mod(x,y))
+
